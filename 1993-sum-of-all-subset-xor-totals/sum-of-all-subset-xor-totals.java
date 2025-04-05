d8149c4ad395779ff968e9dc139889ca2fdeb9bf
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-04-05 22:14:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (45.38%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,21 +1,16 @@
 class Solution {
-    public void func(int idx,int xor,int nums[],int n,ArrayList<Integer>subsetsum){
+    int totalsum=0;
+    public void func(int idx,int xor,int nums[],int n){
         if(idx==n){
-            subsetsum.add(xor);
+            totalsum+=xor;
             return;
         }
-        func(idx+1,xor^nums[idx],nums,n,subsetsum);
-        func(idx+1,xor,nums,n,subsetsum);
+        func(idx+1,xor^nums[idx],nums,n);
+        func(idx+1,xor,nums,n);
     }
     public int subsetXORSum(int[] nums) {
         int n=nums.length;
-        int totalsum=0;
-        ArrayList<Integer>subsetsum=new ArrayList<>();
-        func(0,0,nums,n,subsetsum);
-        for(int i=0;i<subsetsum.size();i++){
-            totalsum+=subsetsum.get(i);
-
-        }
+        func(0,0,nums,n);
         return totalsum;
     }
 }
