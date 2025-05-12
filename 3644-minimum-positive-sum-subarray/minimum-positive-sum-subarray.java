2e1b7d062bcdc238abfa9eb5f3580552904fde34
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-05-13 00:24:22 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (39.44%) - LeetSync

--- 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.java
+++ 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.java
@@ -1,23 +1,28 @@
 class Solution {
     public int minimumSumSubarray(List<Integer> nums, int l, int r) {
-        int n=nums.size();
-        boolean found=false;
-        int minsum=Integer.MAX_VALUE;
-        for(int size=l;size<=r;size++){
-            for(int i=0;i<=n-size;i++){
-                int sum = 0;
-                for(int j=0;j<size;j++){
-                    sum+=nums.get(i+j);
-                }
-                if(sum>0){
-                    found=true;
-                    minsum=Math.min(minsum,sum);
-                }
+        int n = nums.size();
+        int[] prefix = new int[n + 1];
 
+        // Step 1: Build prefix sum
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums.get(i);
+        }
+
+        int minSum = Integer.MAX_VALUE;
+        boolean found = false;
+
+        // Step 2: Try all subarrays of size l to r
+        for (int size = l; size <= r; size++) {
+            for (int i = 0; i <= n - size; i++) {
+                int sum = prefix[i + size] - prefix[i];
+                if (sum > 0) {
+                    found = true;
+                    minSum = Math.min(minSum, sum);
+                }
             }
         }
-        return found?minsum:-1;
-        
+
+        return found ? minSum : -1;
     }
 }
 
