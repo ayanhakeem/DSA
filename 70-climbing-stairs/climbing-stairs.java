6b2c0c88ae9687fcb55ca7cb652e289f8424d6d3
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-05-18 22:16:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (50.64%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    //tabulation tc=0(n) and sc=o(n)
+    public int climbStairs(int n) {
+       int dp[]=new int[n+1];
+       dp[0]=1;
+       for(int i=1;i<=n;i++){
+        if(i==1){
+            dp[i]=dp[i-1]+0;
+        }else{
+             dp[i]=dp[i-1]+dp[i-2];
+
+        }
+       
+       }
+       return dp[n];
+    }
+
+}
