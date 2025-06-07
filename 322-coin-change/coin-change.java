845dc76b34ace0199f57fea0011da59dedb28073
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-06-07 19:12:42 +0530

    Time: 109 ms (7.35%) | Memory: 45 MB (37.77%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,34 +2,36 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
 
-        // Create a 2D array to store results of subproblems
-        int dp[][] = new int[n][amount + 1];
+    
+        int prev[]=new int[amount+1];
+        int curr[]=new int[amount+1];
 
         // Initialize the dp array for the first element of the array
         for (int i = 0; i <= amount; i++) {
-            if (i % coins[0] == 0)
-                dp[0][i] = i / coins[0];
+            if (i % coins[0] == 0)//handle for 0th idx
+                prev[i] = i / coins[0];
             else
-                dp[0][i] = (int) Math.pow(10, 9);
+                prev[i] = (int) Math.pow(10, 9);
         }
 
         // Fill the dp array using dynamic programming
         for (int ind = 1; ind < n; ind++) {
             for (int target = 0; target <= amount; target++) {
-                int notTake = 0 + dp[ind - 1][target];
+                int notTake = 0 + prev[target];
                 int take = (int) Math.pow(10, 9);
 
                 // If the current element is less than or equal to the target, calculate 'take'
                 if (coins[ind] <= target)
-                    take = 1 + dp[ind][target - coins[ind]];
+                    take = 1 +curr[target - coins[ind]];
 
                 // Store the minimum result in the dp array
-                dp[ind][target] = Math.min(notTake, take);
+                curr[target] = Math.min(notTake, take);
             }
+            prev = curr.clone();
         }
 
         // Get the minimum number of elements needed for the target sum
-        int ans = dp[n - 1][amount];
+        int ans = prev[amount];
 
         // If it's not possible to achieve the target sum, return -1
         if (ans >= (int) Math.pow(10, 9))
