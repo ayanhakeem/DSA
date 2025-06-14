60de6ac052ab95dc6697da5cb6a2ddb370f83c8f
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-06-14 18:48:02 +0530

    Time: 4 ms (12.42%) | Memory: 44.6 MB (88.12%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        Map<Integer,Integer>map=new HashMap<>();
+        List<Integer>ans=new ArrayList<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int i:map.keySet()){
+            if(map.get(i)>=2){
+                ans.add(i);
+            }
+        }
+        int res[]=new int[ans.size()];
+        for(int i=0;i<ans.size();i++){
+            res[i]=ans.get(i);
+        }
+        return res;
+        
+    }
+}
