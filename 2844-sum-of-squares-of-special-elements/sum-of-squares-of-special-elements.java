efa64dee54d0ada579f6e369ecbac71d9767d579
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-06-15 22:05:53 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (10.14%) - LeetSync

--- 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.java
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.java
@@ -4,7 +4,7 @@ class Solution {
         List<Integer>ans=new ArrayList<>();
         for(int i=1;i<=n;i++){
             if(n%i==0){
-                ans.add(nums[i-1]);
+                ans.add(nums[i-1]);//0 idx so i-1
             }
         }
         int sum=0;
