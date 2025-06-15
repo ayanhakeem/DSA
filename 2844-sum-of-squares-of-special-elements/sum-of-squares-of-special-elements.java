fec6219f817e67082b59da9804a6e9673a5eacde
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-06-15 22:05:43 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (64.90%) - LeetSync

---
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        int n=nums.length;
+        List<Integer>ans=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                ans.add(nums[i-1]);
+            }
+        }
+        int sum=0;
+        for(int i:ans){
+            sum+=i*i;
+        }
+        return sum;
+        
+    }
+}
