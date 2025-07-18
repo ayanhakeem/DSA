a40ba48a419a56adb0e4e964da92613b8891ed00
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-07-18 12:38:22 +0530

    Time: 22 ms (82.78%) | Memory: 43.3 MB (92.61%) - LeetSync

---
+++ 1187-print-foobar-alternately/print-foobar-alternately.java
@@ -0,0 +1,34 @@
+class FooBar {
+    private final int n;
+     volatile int state = 0;
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                while (state != 0) {
+                    wait();
+                }
+                printFoo.run();
+                state = 1;
+                notifyAll();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                while (state != 1) {
+                    wait();
+                }
+                printBar.run();
+                state = 0;
+                notifyAll();
+            }
+        }
+    }
+}
