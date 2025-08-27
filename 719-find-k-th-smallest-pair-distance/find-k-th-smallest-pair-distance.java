14d873348c4156f31212b88d4a54cb06832b8ddb
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-08-27 12:14:59 +0530

    Time: 6 ms (95.67%) | Memory: 44.8 MB (31.01%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -1,4 +1,40 @@
-class Solution {
+class Solution {//sliding window
+    public int slidingcount(int nums[],int d){
+        int i=0,j=1,n=nums.length,count=0;
+        while(j<n){
+            if(nums[j]-nums[i]>d){
+                i++;
+            }else{
+                count+=j-i;//pairs left side all <=d
+                j++;
+            }
+        }
+        return count;
+    }
+    public int smallestDistancePair(int[] nums, int k) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int l=0,r=nums[n-1]-nums[0];
+        int res=0;
+        while(l<=r){//binary search
+            int mid=l+(r-l)/2;
+            int countpair=slidingcount(nums,mid);
+            if(countpair<k){
+                l=mid+1;
+            }else{
+                res=mid;//store res as possible pair kth smallest
+                r=mid-1;
+
+            }
+        }
+        return res;
+    }
+}
+//tc=o(nlogn)+o(n log m)
+//sc=o(1)
+
+//using dist count how many pairs have of distance
+/*class Solution {
     public int smallestDistancePair(int[] nums, int k) {
         int n=nums.length;
         int max=Integer.MIN_VALUE;  
@@ -21,4 +57,4 @@ class Solution {
         return -1;
         
     }
-}
+}*/
