fca9fa841ae971994d63eef797b4afd2525c872a
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-08-28 23:15:07 +0530

    Time: 3 ms (98.84%) | Memory: 56.1 MB (22.23%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -2,16 +2,16 @@ class Solution {
     public int longestNiceSubarray(int[] nums) {
         int n = nums.length;
         int maxLen = 0;
-        int used = 0; // bitmask of current subarray
+        int mask = 0; // bitmask of current subarray
         int i = 0;
         
         for (int j = 0; j < n; j++) {
-            while ((used & nums[j]) != 0) { 
-                used ^= nums[i]; // remove leftmost
+            while ((mask & nums[j]) != 0) { //keep shrinking window until ==0
+                mask ^= nums[i]; // remove leftmost for remove xor operation used
                 i++;
             }
-            used |= nums[j];
-            maxLen = Math.max(maxLen, j - i + 1);
+            mask |= nums[j];//if ==0 add into mask or opertion
+            maxLen = Math.max(maxLen, j - i + 1);//update len
         }
         
         return maxLen;
