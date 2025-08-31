c11b6a496b71a210c1c7e45d1d3c0fe1e9c6f076
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-08-31 22:30:19 +0530

    Time: 2 ms (70.46%) | Memory: 45.3 MB (29.16%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.java
+++ 4008-restore-finishing-order/restore-finishing-order.java
@@ -1,21 +1,33 @@
 class Solution {
     public int[] recoverOrder(int[] order, int[] friends) {
-        int n=order.length;
-        ArrayList<Integer>ans=new ArrayList<>();
-        Set<Integer>set=new HashSet<>();
-        for(int i:friends){
-            set.add(i);
-        }
-        for(int i=0;i<n;i++){
-            if(set.contains(order[i])){
-                ans.add(order[i]);
+        int n1 = order.length, n2 = friends.length;
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n1; i++) {
+            int rotations = 0;
+            boolean found = false;
+            int j = 0;
+
+            while (rotations < 2 && !found) {
+                if (order[i] == friends[j]) {
+                    arr.add(order[i]);
+                    found = true;
+                } else {
+                    j++;
+                    if (j == n2) { // one rotation completed
+                        j = 0;
+                        rotations++;
+                    }
+                }
             }
+            // if not found, just skip (i++ handled by loop)
         }
-        int arr[]=new int[ans.size()];
-        for(int i=0;i<ans.size();i++){
-            arr[i]=ans.get(i);
+
+        // convert to int[]
+        int[] ans = new int[arr.size()];
+        for (int k = 0; k < arr.size(); k++) {
+            ans[k] = arr.get(k);
         }
-        return arr;
-        
+        return ans;
     }
 }
