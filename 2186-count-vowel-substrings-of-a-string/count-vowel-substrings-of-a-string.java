a929fe429698f06f9d3d6ec4f45700dbace7e226
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-09-12 00:50:24 +0530

    Time: 19 ms (13.09%) | Memory: 42.9 MB (21.83%) - LeetSync

--- 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.java
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.java
@@ -1,35 +1,30 @@
 class Solution {
-    private boolean isVowel(char c) {
+       private boolean isVowel(char c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-    }
-
-    
-    private boolean isVowelSubstring(String s) {
-        Set<Character> vowels = new HashSet<>();
-        for (char c : s.toCharArray()) {
-            if (!isVowel(c)) {
-                return false; 
-            }
-            vowels.add(c);
-        }
-        return vowels.size() == 5;
     }
 
     public int countVowelSubstrings(String word) {
         int n = word.length();
         int count = 0;
-
-       
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j <= n; j++) {
-                String substring = word.substring(i, j);
-                if (isVowelSubstring(substring)) {
-                    count++;
+        
+        for (int start = 0; start < n; start++) {
+            Map<Character, Integer> vowelCount = new HashMap<>();
+            
+            for (int end = start; end < n; end++) {
+                char c = word.charAt(end);
+                
+                if (!isVowel(c)) {
+                    break; // Non-vowel found, reset
+                }
+                
+                vowelCount.put(c, vowelCount.getOrDefault(c, 0) + 1);
+                
+                if (vowelCount.size() == 5) {
+                    count++; // All vowels found, count this substring
                 }
             }
         }
         
         return count;
     }
-
 }
