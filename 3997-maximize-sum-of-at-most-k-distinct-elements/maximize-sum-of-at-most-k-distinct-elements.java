e21eaf9cd52100ae6d618bb86373072785b056e3
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-09-14 11:14:16 +0530

    Time: 10 ms (100.00%) | Memory: 45.5 MB (100.00%) - LeetSync

--- 3997-maximize-sum-of-at-most-k-distinct-elements/maximize-sum-of-at-most-k-distinct-elements.java
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/maximize-sum-of-at-most-k-distinct-elements.java
@@ -2,27 +2,29 @@ import java.util.*;
 class Solution {
     public int[] maxKDistinct(int[] nums, int k) {
           
-        Integer[] nums2 = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-        Arrays.sort(nums2, Collections.reverseOrder());
-
-        Set<Integer> seen = new HashSet<>();
-        List<Integer> ansList = new ArrayList<>();
-
-        for (int num : nums2) {
-            if (!seen.contains(num)) {
-                seen.add(num);
-                ansList.add(num);
-            }
-            if (ansList.size() == k) {
-                break;
-            }
+             // Use a Set to store distinct elements
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
         }
 
-        
-        
+        // Use a max heap (priority queue with reverse order)
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        maxHeap.addAll(set);
 
-      
-        int[] ans = ansList.stream().mapToInt(Integer::intValue).toArray();
+        // Extract up to k elements
+        List<Integer> ansList = new ArrayList<>();
+        
+        while (ansList.size() < k && !maxHeap.isEmpty()) {
+            ansList.add(maxHeap.poll());
+        }
+
+        // Convert list to array
+        int[] ans = new int[ansList.size()];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = ansList.get(i);
+        }
+
         return ans;
     }
      
