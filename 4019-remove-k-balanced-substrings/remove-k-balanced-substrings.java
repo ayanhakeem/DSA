200f6869dc998aa87d8e22dded625d601ef6031d
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-10-09 21:14:12 +0530

    Time: 189 ms (52.42%) | Memory: 46.1 MB (73.01%) - LeetSync

---
+++ 4019-remove-k-balanced-substrings/remove-k-balanced-substrings.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String removeSubstring(String s, int k) {
+        StringBuilder str = new StringBuilder();
+        int openCount = 0;
+
+        for (char c : s.toCharArray()) {
+            str.append(c);
+            if (c == '(') {
+                openCount++;
+            } else {
+                if (str.length() >= 2 * k && openCount >= k) {
+                    int len = str.length();
+                    boolean valid = true;
+
+                    for (int i = len - 2 * k; i < len - k; i++) {
+                        if (str.charAt(i) != '(') {
+                            valid = false;
+                            break;
+                        }
+                    }
+                    for (int i = len - k; i < len; i++) {
+                        if (str.charAt(i) != ')') {
+                            valid = false;
+                            break;
+                        }
+                    }
+
+                    if (valid) {
+                        str.setLength(str.length() - 2 * k);
+                        openCount -= k;
+                    }
+                }
+            }
+        }
+
+        return str.toString();
+    }
+}
