5e4eb5776a54e7a18905b62e98061c13d310f712
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-10-19 15:53:18 +0530

    Time: 395 ms (0.00%) | Memory: 45.3 MB (50.00%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -6,17 +6,18 @@ class Solution {
         int max = 0;
 
         for (int i = 0; i < n; i++) {
-            Set<Integer> evenset = new HashSet<>();
-            Set<Integer> oddset = new HashSet<>();
+            Set<Integer> set = new HashSet<>();
             int even = 0, odd = 0;
 
             for (int j = i; j < n; j++) {
-
                 
-                if (nums[j] % 2 == 0) evenset.add(nums[j]);
-                else oddset.add(nums[j]);
+                if (!set.contains(nums[j])) {
+                    set.add(nums[j]);
+                    if (nums[j] % 2 == 0) even++;
+                    else odd++;
+                }
 
-                if (evenset.size() == oddset.size()) {
+                if (even == odd) {
                     max = Math.max(max, j - i + 1);
                 }
             }
