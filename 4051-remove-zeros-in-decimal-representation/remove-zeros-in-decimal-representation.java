105ecf9b062560cf9f49f9eefba3ef0db5a9b442
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-10-26 18:26:07 +0530

    Time: 3 ms (0.00%) | Memory: 42 MB (0.00%) - LeetSync

--- 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
+++ 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
@@ -1,15 +1,50 @@
 class Solution {
     public long removeZeros(long n) {
-        String s=String.valueOf(n);
+        Stack<Long>stack=new Stack<>();
         StringBuilder sb=new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='0'){
+        while(n>0){
+            long lastdigit=n%10;
+            n=n/10;
+            if(lastdigit==0){
                 continue;
+
             }else{
-                sb.append(s.charAt(i));
+                stack.push(lastdigit);
             }
         }
-        return  Long.parseLong(sb.toString());
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return Long.parseLong(sb.toString());
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public long removeZeros(long n) {
+//         String s=String.valueOf(n);
+//         StringBuilder sb=new StringBuilder();
+//         for(int i=0;i<s.length();i++){
+//             if(s.charAt(i)=='0'){
+//                 continue;
+//             }else{
+//                 sb.append(s.charAt(i));
+//             }
+//         }
+//         return  Long.parseLong(sb.toString());
+        
+//     }
+// }
