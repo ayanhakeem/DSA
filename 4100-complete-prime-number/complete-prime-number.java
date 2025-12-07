e3a032eab9fb55a3f89f39441ea329a4e0c6a385
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-12-07 19:04:19 +0530

    Time: 2 ms (100.00%) | Memory: 43.1 MB (0.00%) - LeetSync

--- 4100-complete-prime-number/complete-prime-number.java
+++ 4100-complete-prime-number/complete-prime-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean primenum(int n){
         if(n<=1) return false;
-        for(int i=2;i<=n/2;i++){
+        for(int i=2;i*i<=n;i++){
             if(n%i==0) return false;
 
         }
@@ -27,3 +27,5 @@ class Solution {
         return true;
     }
 }
+
+//tc=
