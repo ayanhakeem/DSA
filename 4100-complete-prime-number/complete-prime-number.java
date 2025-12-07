192d1ec7280c48c4620e8ce57a388731eed8c2ce
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-12-07 19:03:57 +0530

    Time: 5 ms (0.00%) | Memory: 43 MB (0.00%) - LeetSync

--- 4100-complete-prime-number/complete-prime-number.java
+++ 4100-complete-prime-number/complete-prime-number.java
@@ -1,28 +1,28 @@
 class Solution {
+    public boolean primenum(int n){
+        if(n<=1) return false;
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0) return false;
 
-    public boolean isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
         }
         return true;
     }
-
     public boolean completePrime(int num) {
-        String s = String.valueOf(num);
-        int n = s.length();
-
-        // Check all prefixes
-        for (int i = 1; i <= n; i++) {
-            int prefix = Integer.parseInt(s.substring(0, i));
-            if (!isPrime(prefix)) return false;
+        
+        String s=String.valueOf(num);
+        int n=s.length();
+        //check for prefix one by one
+        for(int i=1;i<=n;i++){
+            int prefix=Integer.parseInt(s.substring(0,i));
+            if(!primenum(prefix)) return false;
+        }
+        //check for suffix 
+        for(int i=0;i<n;i++){
+            int suffix=Integer.parseInt(s.substring(i));
+            if(!primenum(suffix)) return false;
         }
 
-        // Check all suffixes
-        for (int i = 0; i < n; i++) {
-            int suffix = Integer.parseInt(s.substring(i));
-            if (!isPrime(suffix)) return false;
-        }
+        
 
         return true;
     }
