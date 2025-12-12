27cffb2c4ae0afdae89277d5b305e39152bbbc8e
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-12-12 19:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (26.81%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] searchRange(int[] nums, int target) {
         int ans[]={-1,-1};
         int n=nums.length;
-        if(n==1 && nums[0]==target){
+        if(n==1 && nums[0]==target){//if nums length is 1 and nums[0]==target then store ans[0]=0 
             ans[0]=0;
             ans[1]=0;
             return ans;
@@ -11,12 +11,12 @@ class Solution {
         int low=0,high=n-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                int l=mid;
-                while(l>=low && nums[l]==target) l--;//search left occurance
+            if(nums[mid]==target){//if mid==target then search on left is there any occurences means first occurence bcoz array is sorted means elements equal to target lies consecutive
+                int l=mid;//assign var l as mid 
+                while(l>=low && nums[l]==target) l--;//search left occurance means first
                 ans[0]=l+1;
                 int r=mid;
-                while(r<=high && nums[r]==target) r++;//search right occurance
+                while(r<=high && nums[r]==target) r++;//search right occurance means last
                 ans[1]=r-1;
                 return ans;
             }else if(nums[mid]<target){
@@ -30,3 +30,6 @@ class Solution {
         
     }
 }
+//use binary search on sorted array 
+//tc=o(logn)
+//sc=o(1)
