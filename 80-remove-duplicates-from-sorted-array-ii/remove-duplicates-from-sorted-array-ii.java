3db4b1b2a4dc9c8afae584145c76226bdd65ef33
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-12-13 18:29:21 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (61.70%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,30 +1,57 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        LinkedHashMap<Integer,Integer>map=new LinkedHashMap<>();
         int n=nums.length;
-
-        for(int i=0;i<n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-
-        int k=0;
-        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
-            int v=entry.getKey();
-            int f=Math.min(2,entry.getValue());
-
-            for(int i=0;i<f;i++){
-                nums[k++]=v;
+        if(n<=2) return n;
+        int k=2;
+        for(int i=2;i<n;i++){
+            if(nums[i]!=nums[k-2]){
+                nums[k++]=nums[i];
             }
+        }
+        return k;
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//brute force tc=o(n)+o(n^2) sc=o(n)
+
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         LinkedHashMap<Integer,Integer>map=new LinkedHashMap<>();
+//         int n=nums.length;
+
+//         for(int i=0;i<n;i++){
+//             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+//         }
+
+//         int k=0;
+//         for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+//             int v=entry.getKey();
+//             int f=Math.min(2,entry.getValue());
+
+//             for(int i=0;i<f;i++){
+//                 nums[k++]=v;
+//             }
 
             
-        }
+//         }
 
-        return k;
+//         return k;
 
 
 
 
 
         
-    }
-}
+//     }
+// }
