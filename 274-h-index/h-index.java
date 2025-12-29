d2910b5cc3adbedf17f19c8a3cd6c75ad9d44e0a
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-12-29 20:10:50 +0530

    Time: 7 ms (8.40%) | Memory: 43.1 MB (85.15%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean atleast(int h,int citations[]){
+        int n=citations.length;
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(citations[i]>=h){
+                c++;
+            }
+        }
+        if(c>=h) return true;
+
+        return false;
+
+    }
+    public int hIndex(int[] citations) {
+        int n=citations.length;
+        int cnt=0;
+        for(int h=1;h<1000;h++){//citations
+            if(atleast(h,citations)){//papers
+                cnt++;
+            }
+        }
+        return cnt;
+
+    }
+}
