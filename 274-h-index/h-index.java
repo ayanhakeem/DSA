958976ae8ac36f79d7cd2c01220646d7fe89d3ce
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2025-12-30 00:26:00 +0530

    Time: 6 ms (21.42%) | Memory: 43.5 MB (51.48%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,26 +1,52 @@
 class Solution {
-    public boolean atleast(int h,int citations[]){
-        int n=citations.length;
-        int c=0;
-        for(int i=0;i<n;i++){
-            if(citations[i]>=h){
-                c++;
-            }
-        }
-        if(c>=h) return true;
-
-        return false;
-
-    }
     public int hIndex(int[] citations) {
         int n=citations.length;
-        int cnt=0;
-        for(int h=1;h<1000;h++){//citations
-            if(atleast(h,citations)){//papers
-                cnt++;
+        Arrays.sort(citations);
+        for(int i=0;i<n;i++){
+            int papers=n-i;//right side papers
+            if(papers<=citations[i]){
+                return papers;
             }
         }
-        return cnt;
-
+        return 0;
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//brute force
+// class Solution {
+//     public boolean atleast(int h,int citations[]){
+//         int n=citations.length;
+//         int c=0;
+//         for(int i=0;i<n;i++){
+//             if(citations[i]>=h){
+//                 c++;
+//             }
+//         }
+//         if(c>=h) return true;
+
+//         return false;
+
+//     }
+//     public int hIndex(int[] citations) {
+//         int n=citations.length;
+//         int cnt=0;
+//         for(int h=1;h<1000;h++){//citations
+//             if(atleast(h,citations)){//papers
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+
+//     }
+// }
