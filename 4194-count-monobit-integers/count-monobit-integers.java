26ae17dbd69e9ce2b061d1ba3c01a65f3b75c854
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-02-01 16:43:27 +0530

    Time: 1 ms (99.41%) | Memory: 42.5 MB (91.27%) - LeetSync

--- 4194-count-monobit-integers/count-monobit-integers.java
+++ 4194-count-monobit-integers/count-monobit-integers.java
@@ -1,19 +1,39 @@
 class Solution {
-    private static boolean ismonotonic(String s){
-        Set<Character>set=new HashSet<>();
-        for(int i=0;i<s.length();i++){
-            set.add(s.charAt(i));
-        }
-        return set.size() == 1;
-    }
     public int countMonobit(int n) {
-        int cnt=0;
-        for(int i=0;i<=n;i++){
-            String binary = Integer.toBinaryString(i);
-            if(ismonotonic(binary)){
-                cnt++;
-            } 
+        int cnt=1;//for 0 monobit
+        int num=1;
+        while(num<=n){
+            cnt++;
+            num=(num<<1) | 1;
         }
+
         return cnt;
+        
     }
 }
+
+
+
+
+
+
+
+// class Solution {
+//     private static boolean ismonotonic(String s){
+//         Set<Character>set=new HashSet<>();
+//         for(int i=0;i<s.length();i++){
+//             set.add(s.charAt(i));
+//         }
+//         return set.size() == 1;
+//     }
+//     public int countMonobit(int n) {
+//         int cnt=0;
+//         for(int i=0;i<=n;i++){
+//             String binary = Integer.toBinaryString(i);
+//             if(ismonotonic(binary)){
+//                 cnt++;
+//             } 
+//         }
+//         return cnt;
+//     }
+// }
