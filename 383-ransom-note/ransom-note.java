f05ffa6d3aec6b16dc0a58503b2892be7635ecc7
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-02-07 23:45:35 +0530

    Time: 1 ms (99.76%) | Memory: 46.6 MB (51.58%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,19 +1,41 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int hash1[]=new int[128];
-        for(int i=0;i<magazine.length();i++){
-            char c=magazine.charAt(i);
-            hash1[c]++;
-        }
-        int hash2[]=new int[128];
-        for(int i=0;i<ransomNote.length();i++){
-            char c=ransomNote.charAt(i);
-            hash2[c]++;
+        int hash[]=new int[26];
+        for(char c:magazine.toCharArray()){
+            hash[c-'a']++;
         }
 
-        for(int i=0;i<128;i++){
-            if(hash2[i]>hash1[i]) return false;
+        for(char c:ransomNote.toCharArray()){
+            if(hash[c-'a']==0) return false;
+            hash[c-'a']--;
         }
         return true;
+        
     }
 }
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         int hash1[]=new int[128];
+//         for(int i=0;i<magazine.length();i++){
+//             char c=magazine.charAt(i);
+//             hash1[c]++;
+//         }
+//         int hash2[]=new int[128];
+//         for(int i=0;i<ransomNote.length();i++){
+//             char c=ransomNote.charAt(i);
+//             hash2[c]++;
+//         }
+
+//         for(int i=0;i<128;i++){
+//             if(hash2[i]>hash1[i]) return false;
+//         }
+//         return true;
+//     }
+// }
