4051b759814036c02a839d600e9dba4a961a11d7
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-02-09 23:46:34 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (0.00%) - LeetSync

---
+++ 4214-count-dominant-indices/count-dominant-indices.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static boolean avgsum(int st,int nums[]){
+        int n=nums.length;
+        int sum=0;
+        for(int i=st+1;i<n;i++){
+            sum+=nums[i];
+        }
+        int totele=n-st-1;
+        float avg=(float)sum/totele;
+        return nums[st]>avg;
+    }
+    public int dominantIndices(int[] nums) {
+        int n=nums.length;
+        int cnt=0;
+        for(int i=n-2;i>=0;i--){
+            if(avgsum(i,nums)){
+                cnt++;
+            }
+        }
+        return cnt;
+        
+    }
+}
