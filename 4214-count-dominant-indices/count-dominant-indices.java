41a9f23c463620b6c9cce2a8428f4f2eb4c4f1d4
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-02-09 23:52:32 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (0.00%) - LeetSync

--- 4214-count-dominant-indices/count-dominant-indices.java
+++ 4214-count-dominant-indices/count-dominant-indices.java
@@ -1,23 +1,39 @@
 class Solution {
-    public static boolean avgsum(int st,int nums[]){
-        int n=nums.length;
-        int sum=0;
-        for(int i=st+1;i<n;i++){
-            sum+=nums[i];
-        }
-        int totele=n-st-1;
-        float avg=(float)sum/totele;
-        return nums[st]>avg;
-    }
     public int dominantIndices(int[] nums) {
         int n=nums.length;
+        int sum=0;
         int cnt=0;
-        for(int i=n-2;i>=0;i--){
-            if(avgsum(i,nums)){
-                cnt++;
-            }
+        for(int i=n-1;i>0;i--){
+            sum+=nums[i];
+            if(nums[i-1]>sum/(n-i)) cnt++;
         }
         return cnt;
         
     }
 }
+
+// class Solution {
+//     public static boolean avgsum(int st,int nums[]){
+//         int n=nums.length;
+//         int sum=0;
+//         for(int i=st+1;i<n;i++){
+//             sum+=nums[i];
+//         }
+//         int totele=n-st-1;
+//         float avg=(float)sum/totele;
+//         return nums[st]>avg;
+//     }
+//     public int dominantIndices(int[] nums) {
+//         int n=nums.length;
+//         int cnt=0;
+//         for(int i=n-2;i>=0;i--){
+//             if(avgsum(i,nums)){
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+        
+//     }
+// }
+//tc=o(n^2)
+//sc=o(1)
