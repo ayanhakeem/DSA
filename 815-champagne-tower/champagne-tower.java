ab54a84717f1ef3241da7b8f3a5f7ee3bfc7b246
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-02-14 19:25:03 +0530

    Time: 2 ms (98.59%) | Memory: 48 MB (83.04%) - LeetSync

--- 815-champagne-tower/champagne-tower.java
+++ 815-champagne-tower/champagne-tower.java
@@ -1,22 +1,19 @@
 class Solution {
     public double champagneTower(int poured, int query_row, int query_glass) {
-        
-        double[][] dp = new double[query_row + 2][query_row + 2];
-        dp[0][0] = poured;
-        
-        for (int r = 0; r <= query_row; r++) {
-            for (int c = 0; c <= r; c++) {
-                
-                if (dp[r][c] > 1.0) {
-                    double overflow = dp[r][c] - 1.0;
-                    dp[r][c] = 1.0;  // glass can hold only 1
-                    
-                    dp[r + 1][c] += overflow / 2.0;
-                    dp[r + 1][c + 1] += overflow / 2.0;
+        double dp[][]=new double[query_row+2][query_row+2];///extra for overflow safety
+        dp[0][0]=poured;
+        for(int r=0;r<=query_row;r++){
+            for(int c=0;c<=r;c++){
+                if(dp[r][c]>1.0){
+                    double overflow=dp[r][c]-1.0;
+                    dp[r][c]=1.0;
+                    dp[r+1][c]+=overflow/2.0;
+                    dp[r+1][c+1]+=overflow/2.0;
                 }
             }
         }
-        
         return dp[query_row][query_glass];
+        
+        
     }
 }
