3962813227448293162eb906054357d410fbf39d
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-02-21 22:12:03 +0530

    Time: 4 ms (20.92%) | Memory: 46.5 MB (83.89%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -1,36 +1,40 @@
 class Solution {
-    static class row implements Comparable<row>{
-        int soldiers;
-        int idx;
+    // static class row implements Comparable<row>{
+    //     int soldiers;
+    //     int idx;
 
-        public row(int soldiers,int idx){
-            this.soldiers=soldiers;
-            this.idx=idx;
-        }
+    //     public row(int soldiers,int idx){
+    //         this.soldiers=soldiers;
+    //         this.idx=idx;
+    //     }
 
-        @Override
-        public int compareTo(row r2){
-            if(this.soldiers==r2.soldiers){
-                return this.idx-r2.idx;
-            }
-            else{
-                return this.soldiers-r2.soldiers;
-            }
-        }
-    }
+    //     @Override
+    //     public int compareTo(row r2){
+    //         if(this.soldiers==r2.soldiers){
+    //             return this.idx-r2.idx;
+    //         }
+    //         else{
+    //             return this.soldiers-r2.soldiers;
+    //         }
+    //     }
+    // }
     public int[] kWeakestRows(int[][] mat, int k) {
-        PriorityQueue<row>pq=new PriorityQueue<>();
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->{
+            if(a[0]==b[0]) return a[1]-b[1];
+            return a[0]-b[0];
+        });
         for(int i=0;i<mat.length;i++){
             int cnt=0;
             for(int j=0;j<mat[0].length;j++){
                 cnt+=mat[i][j]==1?1:0;
             }
-            pq.add(new row(cnt,i));
+            pq.add(new int[]{cnt,i});
         }
         int ans[]=new int[k];
         int j=0;
         for(int i=0;i<k;i++){
-            ans[j++]=pq.remove().idx;
+            int pqq[]=pq.remove();
+            ans[j++]=pqq[1];
         }
         return ans;
         
