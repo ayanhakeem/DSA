3f57ec3c6b6ecd91613a8fe18cafc2b6c4c7cefd
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-02-28 22:17:31 +0530

    Time: 19 ms (89.81%) | Memory: 46.7 MB (93.86%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,18 +1,60 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int n=text1.length();
-        int m=text2.length();
-        int dp[][]=new int[n+1][m+1];
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]+1;//same adding 1 for same maeans length
-                }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);//diff
+
+        int n = text1.length();
+        int m = text2.length();
+
+        int prev[] = new int[m + 1];
+        int curr[] = new int[m + 1];
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= m; j++) {
+
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    curr[j] = prev[j - 1] + 1;
+                } else {
+                    curr[j] = Math.max(prev[j], curr[j - 1]);
                 }
             }
+
+            prev = curr.clone(); // move current row to previous
         }
-        return dp[n][m];
-        
+
+        return prev[m];
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         int n=text1.length();
+//         int m=text2.length();
+//         int dp[][]=new int[n+1][m+1];
+//         for(int i=1;i<n+1;i++){
+//             for(int j=1;j<m+1;j++){
+//                 if(text1.charAt(i-1)==text2.charAt(j-1)){
+//                     dp[i][j]=dp[i-1][j-1]+1;//same adding 1 for same maeans length
+//                 }else{
+//                     dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);//diff
+//                 }
+//             }
+//         }
+//         return dp[n][m];
+        
+//     }
+// }
