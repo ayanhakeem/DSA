b964610c60fb1eaff370a91b2775d6fec5da3f41
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-03-01 10:11:03 +0530

    Time: 27 ms (50.97%) | Memory: 47.5 MB (53.51%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+       public int lcs(String text1, String text2) {
+        int n=text1.length();
+        int m=text2.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;//same adding 1 for same maeans length
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);//diff
+                }
+            }
+        }
+        return dp[n][m];
+        
+    }
+    public int longestPalindromeSubseq(String s) {
+        String t = new StringBuilder(s).reverse().toString();//take the commane lcs of t and s means t is reverse of s then we can get lps
+        return lcs(s,t);
+        
+    }
+
+    public int minInsertions(String s) {
+        return s.length()-longestPalindromeSubseq(s);
+        
+    }
+}
