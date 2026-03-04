123932494164528d2cba35c4cb62bc90d899953c
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-03-04 13:53:41 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (86.79%) - LeetSync

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,12 +1,39 @@
 class Solution {
     public double average(int[] salary) {
-        int n=salary.length;
-        Arrays.sort(salary);
-        double sum=0;
-        for(int i=1;i<n-1;i++){
-            sum+=salary[i];
+        int n = salary.length;
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        double sum = 0;
+
+        for(int i = 0; i < n; i++){
+            max = Math.max(max, salary[i]);
+            min = Math.min(min, salary[i]);
+            sum += salary[i];
         }
-        double avg=sum/(n-2);
-        return avg;
+
+        sum = sum - max - min;
+
+        return sum / (n - 2);
     }
 }
+
+
+
+
+
+
+
+
+// class Solution {
+//     public double average(int[] salary) {
+//         int n=salary.length;
+//         Arrays.sort(salary);
+//         double sum=0;
+//         for(int i=1;i<n-1;i++){
+//             sum+=salary[i];
+//         }
+//         double avg=sum/(n-2);
+//         return avg;
+//     }
+// }
