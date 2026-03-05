34d3ac0f907ca03e57793e5a37444042f5137c67
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-03-05 12:06:24 +0530

    Time: 5 ms (53.53%) | Memory: 43.7 MB (79.17%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minOperations(String s) {
+        int oddp0 =0;
+        int oddp1 =0;
+        if(s.length()==1)return 0;
+        //1010
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(i%2==0){
+                if(ch=='0')oddp0++;
+            }
+            else{
+                if(ch=='1')oddp0++;
+            }
+        }
+        //0101
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(i%2==1){
+                if(ch=='0')oddp1++;
+            }
+            else{
+                if(ch=='1')oddp1++;
+            }
+        }
+        return Math.min(oddp1, oddp0);
+    }
+}
