260bd6771bdb148be9d3ae789830a2718e9381ea
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-03-11 19:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (66.82%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,17 +1,32 @@
 class Solution {
     public int bitwiseComplement(int n) {
         if(n==0) return 1;
-       String s = Integer.toBinaryString(n);
-       
-       StringBuilder sb=new StringBuilder();
-       for(int i=0;i<s.length();i++){
-        if(s.charAt(i)=='0'){
-            sb.append('1');
-        }else{
-            sb.append('0');
+        int mask=1;
+        while(mask<n){
+            mask=(mask<<1)|1;
+
         }
-       }
-       int num = Integer.parseInt(sb.toString(), 2);
-       return num;
+        return mask^n;
     }
 }
+
+
+
+//brute force
+// class Solution {
+//     public int bitwiseComplement(int n) {
+//         if(n==0) return 1;
+//        String s = Integer.toBinaryString(n);//o(logn)
+       
+//        StringBuilder sb=new StringBuilder();//o(logn)
+//        for(int i=0;i<s.length();i++){//o(logn)
+//         if(s.charAt(i)=='0'){
+//             sb.append('1');
+//         }else{
+//             sb.append('0');
+//         }
+//        }
+//        int num = Integer.parseInt(sb.toString(), 2);//o(logn)
+//        return num;
+//     }
+// }
