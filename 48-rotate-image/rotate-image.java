30cfa52c1b97ea9075a95b2ae57cad33629dcf16
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-05-04 18:43:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (69.68%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,31 +1,34 @@
 class Solution {
-    public void swap(int matrix[][],int i,int j){
-        int temp=matrix[i][j];
-        matrix[i][j]=matrix[j][i];
-        matrix[j][i]=temp;
-    }
-    public void reverse(int[] row) {
-        int left = 0, right = row.length - 1;
-        while (left < right) {
-            int temp = row[left];
-            row[left] = row[right];
-            row[right] = temp;
-            left++;
-            right--;
-        }
+    public void swap(int mat[][],int i,int j){
+        int temp=mat[i][j];
+        mat[i][j]=mat[j][i];
+        mat[j][i]=temp;
     }
 
+    public void reverse(int row[]){
+        int n=row.length;
+        int l=0,r=n-1;
+        while(l<r){
+            int temp=row[l];
+            row[l]=row[r];
+            row[r]=temp;
+            l++;
+            r--;
+        }
+
+    }
     public void rotate(int[][] matrix) {
         int n=matrix.length;
+        int m=matrix[0].length;
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
                 swap(matrix,i,j);
             }
         }
-       for(int i=0;i<n;i++){
-         reverse(matrix[i]);
-       }
-    
-        
+
+        for(int i=0;i<n;i++){
+            reverse(matrix[i]);
+        }
+
     }
 }
