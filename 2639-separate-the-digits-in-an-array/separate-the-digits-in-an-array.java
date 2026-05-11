73214bf74fc78d151be0249ee122afd6a9e36204
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-05-11 18:27:57 +0530

    Time: 10 ms (10.77%) | Memory: 47.3 MB (7.08%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
@@ -1,39 +1,41 @@
-class Solution {
-    public int[] separateDigits(int[] nums) {
-       int l=nums.length;
-       List<Integer>ans=new ArrayList<>();
-       for(int i=0;i<l;i++){
-        int n=nums[i];
-        String s=Integer.toString(n);
-        for(int j=0;j<s.length();j++){
-            int x=s.charAt(j)-'0';
-            ans.add(x);
-        }
-       }
-       int[] arr = ans.stream().mapToInt(Integer::intValue).toArray();
-       return arr;
-       
-
-    }
-}
-
-
 // class Solution {
 //     public int[] separateDigits(int[] nums) {
-//         int l=nums.length;
-//         List<Integer>ans=new ArrayList<>();
-//         for(int i=0;i<l;i++){
-//             int n=nums[i];
-//             while(n>0){
-//                 int lastdigit=n%10;
-//                 ans.add(lastdigit);
-//                 n=n/10;
-//             }
-//             Collections.reverse(ans);
+//        int l=nums.length;
+//        List<Integer>ans=new ArrayList<>();
+//        for(int i=0;i<l;i++){
+//         int n=nums[i];
+//         String s=Integer.toString(n);
+//         for(int j=0;j<s.length();j++){
+//             int x=s.charAt(j)-'0';
+//             ans.add(x);
 //         }
-//         int[] arr = ans.stream().mapToInt(Integer::intValue).toArray();
-//         return arr;
+//        }
+//        int[] arr = ans.stream().mapToInt(Integer::intValue).toArray();
+//        return arr;
+       
+
 //     }
 // }
+
+
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        int l=nums.length;
+        List<Integer>ans=new ArrayList<>();
+        for(int i=0;i<l;i++){
+            List<Integer> temp = new ArrayList<>();
+            int n=nums[i];
+            while(n>0){
+                int lastdigit=n%10;
+                temp.add(lastdigit);
+                n=n/10;
+            }
+            Collections.reverse(temp);
+            ans.addAll(temp);
+        }
+        int[] arr = ans.stream().mapToInt(Integer::intValue).toArray();
+        return arr;
+    }
+}
 
 
