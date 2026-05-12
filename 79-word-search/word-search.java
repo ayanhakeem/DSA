b0dfa8ba2a09252cb3bb56d0cdf3f6203b0d635d
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-05-12 20:22:36 +0530

    Time: 260 ms (10.31%) | Memory: 46.9 MB (6.50%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,18 +1,19 @@
 class Solution {
-    public static boolean dfs(int i,int j,int k,int n,int m,char board[][],String word,boolean vis[][]){
+    public static boolean dfs(int i,int j,int k,int n,int m,char board[][],String word){
         
         if(k==word.length()) return true;//we have reached end of word matched 
 
 
         // boundary + visited + mismatch check
         if (i < 0 || i >= n || j < 0 || j >= m
-                || vis[i][j]
+                || board[i][j]=='#'
                 || board[i][j] != word.charAt(k)) {
             return false;
         }
 
-        // mark visited
-        vis[i][j] = true;
+        // store char andmark visited 
+        char temp=board[i][j];
+        board[i][j] = '#';
 
         // 4 directions
         int[][] directions = {
@@ -28,13 +29,13 @@ class Solution {
             int nr = i + d[0];
             int nc = j + d[1];
 
-            if (dfs(nr, nc, k + 1,n,m, board, word, vis)) {
+            if (dfs(nr, nc, k + 1,n,m, board, word)) {
                 return true;
             }
         }
 
         // backtrack
-        vis[i][j] = false;
+        board[i][j] = temp;
 
         return false;
     
@@ -43,11 +44,10 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int n=board.length;
         int m=board[0].length;
-        boolean vis[][]=new boolean[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
 
-                if (dfs(i, j, 0,n,m, board, word, vis)) {
+                if (dfs(i, j, 0,n,m, board, word)) {
                     return true;
                 }
             }
@@ -56,3 +56,69 @@ class Solution {
         return false;
     }
 }
+
+//tc=o(n*m*4)
+//sc=o(n*m)+o(l)recursion stack
+
+
+// class Solution {
+//     public static boolean dfs(int i,int j,int k,int n,int m,char board[][],String word,boolean vis[][]){
+        
+//         if(k==word.length()) return true;//we have reached end of word matched 
+
+
+//         // boundary + visited + mismatch check
+//         if (i < 0 || i >= n || j < 0 || j >= m
+//                 || vis[i][j]
+//                 || board[i][j] != word.charAt(k)) {
+//             return false;
+//         }
+
+//         // mark visited
+//         vis[i][j] = true;
+
+//         // 4 directions
+//         int[][] directions = {
+//                 {1, 0},
+//                 {-1, 0},
+//                 {0, 1},
+//                 {0, -1}
+//         };
+
+//         // explore neighbors
+//         for (int[] d : directions) {
+
+//             int nr = i + d[0];
+//             int nc = j + d[1];
+
+//             if (dfs(nr, nc, k + 1,n,m, board, word, vis)) {
+//                 return true;
+//             }
+//         }
+
+//         // backtrack
+//         vis[i][j] = false;
+
+//         return false;
+    
+
+//     }
+//     public boolean exist(char[][] board, String word) {
+//         int n=board.length;
+//         int m=board[0].length;
+//         boolean vis[][]=new boolean[n][m];
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+
+//                 if (dfs(i, j, 0,n,m, board, word, vis)) {
+//                     return true;
+//                 }
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+
+//tc=o(n*m*4)
+//sc=o(n*m)+o(l)recursion stack
