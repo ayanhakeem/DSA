43b3f70f8b81b94df6bb011526644be316546949
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-05-22 17:15:57 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (19.38%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -1,10 +1,28 @@
+// class Solution {
+//     public int findMin(int[] nums) {
+//         int n=nums.length;
+//         int min=Integer.MAX_VALUE;
+//        for(int i=0;i<n;i++){
+//         min=Math.min(min,nums[i]);
+//        }
+//        return min;
+//     }
+// }
 class Solution {
     public int findMin(int[] nums) {
         int n=nums.length;
-        int min=Integer.MAX_VALUE;
-       for(int i=0;i<n;i++){
-        min=Math.min(min,nums[i]);
-       }
-       return min;
+        int st=0,end=n-1;
+        while(st<end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]>nums[end]){
+                st=mid+1;//min ele lies on right bcoz right side is sorted but not left side partial sorted
+            }else if(nums[mid]<nums[end]){
+                end=mid;
+            }else{
+                //duplicates
+                end--;
+            }
+        }
+        return nums[st];
     }
 }
