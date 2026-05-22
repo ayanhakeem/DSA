193928d1af53b05c82a77f80d4e62cb670c74433
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-05-22 17:04:14 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (79.87%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,41 +1,46 @@
-class Solution {
-    public boolean search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target) return true;
-        }
-
-        return false;
-    }
-}
-
 // class Solution {
 //     public boolean search(int[] nums, int target) {
-//         int si=0;
-//         int ei=nums.length-1;
-//         while(si<=ei){
-//             int mid=si+(ei-si)/2;
-//             if(nums[mid]==target){
-//                 return  true;
-//             }
-//             //left part is sorted
-//             else if(nums[si]<=nums[mid]){
-//                 if(nums[si]<=target && target<=nums[mid]){//si<=target<=mid
-//                 ei=mid-1;
-//                 }else{
-//                     si=mid+1;
-//                 }
-//                 //lest is not sorted right part is sorted
-//             }else{
-//                 if(nums[mid]<=target && target<=nums[ei]){
-//                     si=mid+1;
-//                 }else{
-//                     ei=mid-1;
-//                 }
-//             }
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]==target) return true;
 //         }
 
-//         return false;//not found
+//         return false;
 //     }
 // }
-// //tc=o(logn)
-// //sc=o(1)
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int si=0;
+        int ei=nums.length-1;
+        while(si<=ei){
+            int mid=si+(ei-si)/2;
+            if(nums[mid]==target){
+                return  true;
+            }
+            //skip duplicates
+            if(nums[si] == nums[mid] && nums[mid] == nums[ei]){
+                si++;
+                ei--;
+            }
+            //left part is sorted
+            else if(nums[si]<=nums[mid]){
+                if(nums[si]<=target && target<=nums[mid]){//si<=target<=mid
+                ei=mid-1;
+                }else{
+                    si=mid+1;
+                }
+                //lest is not sorted right part is sorted
+            }else{
+                if(nums[mid]<=target && target<=nums[ei]){
+                    si=mid+1;
+                }else{
+                    ei=mid-1;
+                }
+            }
+        }
+
+        return false;//not found
+    }
+}
+//tc=o(logn)
+//sc=o(1)
