14c7a0c74f6f7d9ce87b11c6572e126a0608b43a
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-05-31 11:52:54 +0530

    Time: 1 ms (99.49%) | Memory: 42.6 MB (75.76%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.java
+++ 4321-digit-frequency-score/digit-frequency-score.java
@@ -1,17 +1,32 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        Map<Integer,Integer>map=new HashMap<>();
+        int sum=0;
         while(n>0){
             int d=n%10;
-            map.put(d,map.getOrDefault(d,0)+1);
+            sum+=d;
             n=n/10;
-        }
-        int sum=0;
-        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
-            int k=entry.getKey();
-            int v=entry.getValue();
-            sum+=k*v;
         }
         return sum;
     }
 }
+
+// class Solution {
+//     public int digitFrequencyScore(int n) {
+//         Map<Integer,Integer>map=new HashMap<>();
+//         while(n>0){
+//             int d=n%10;
+//             map.put(d,map.getOrDefault(d,0)+1);
+//             n=n/10;
+//         }
+//         int sum=0;
+//         for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+//             int k=entry.getKey();
+//             int v=entry.getValue();
+//             sum+=k*v;
+//         }
+//         return sum;
+//     }
+// }
+
+// //tc=o(n)+o(n)
+// //sc=o(n
