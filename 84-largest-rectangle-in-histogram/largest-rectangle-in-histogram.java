5f8a457ef3fdb56b36c3fe815cd077acd816df83
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-05-31 11:03:02 +0530

    Time: 109 ms (17.43%) | Memory: 77.8 MB (69.83%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -3,44 +3,44 @@ class Solution {
         int n=heights.length;
         int nsl[]=new int[n];
         int nsr[]=new int[n];
-        int max=0;
-        Stack<Integer>st=new Stack<>();
+        Stack<Integer>st=new Stack<>();//stores idx not values
 
         //nsr
         for(int i=n-1;i>=0;i--){
-          while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-            st.pop();
-          } 
-          if(st.isEmpty()){
-            nsr[i]=n;
-          }else{
-            nsr[i]=st.peek();
-          }
-          st.push(i);
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
+                st.pop();//if we found any ele with bar ht >= curr bar ht then we pop bcoz we want smaller on the stack 
+            }
+            if(st.isEmpty()){
+                nsr[i]=n;//if st.isempty then we assign as n for smaller whole width will be coverd we havent found any smaller in right for curr
+            }else{
+                nsr[i]=st.peek();//storing idx only not values
+            }
+            st.push(i);//push idx comman in all opearions 
+        }
+         //nsl
+        st=new Stack<>();//make st empty
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
+                st.pop();//if we found any ele with bar ht >= curr bar ht then we pop bcoz we want smaller on the stack 
+            }
+            if(st.isEmpty()){
+                nsl[i]=-1;//if st.isempty then we assign as -1 for smaller whole width we havent found any smaller in left for curr so -1
+            }else{
+                nsl[i]=st.peek();//storing idx only not values
+            }
+            st.push(i);//push idx comman in all opearions 
         }
 
-        st=new Stack<>();
-    //nsl
+        //area for each bar
+        int maxarea=0;
         for(int i=0;i<n;i++){
-          while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-            st.pop();
-          } 
-          if(st.isEmpty()){
-            nsl[i]=-1;
-          }else{
-            nsl[i]=st.peek();
-          }
-          st.push(i);
+            int ht=heights[i];
+            int area=ht*(nsr[i]-nsl[i]-1);
+            maxarea=Math.max(maxarea,area);
         }
-       
-
-        for(int i=0;i<n;i++){
-            int height=heights[i];
-            int width=nsr[i]-nsl[i]-1;
-            int area=height*width;
-            max=Math.max(max,area);
-        }
-        return max;
-        
+        return maxarea;
     }
 }
+
+//tc=o(n)
+//sc=o(n)
