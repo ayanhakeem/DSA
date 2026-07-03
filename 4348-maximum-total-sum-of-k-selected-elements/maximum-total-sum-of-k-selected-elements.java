090d28f0d83cda9685128acea0c1657fc2695ca7
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-03 12:52:13 +0530

    Time: 49 ms (73.77%) | Memory: 149 MB (32.31%) - LeetSync

---
+++ 4348-maximum-total-sum-of-k-selected-elements/maximum-total-sum-of-k-selected-elements.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long maxSum(int[] nums, int k, int mul) {
+        Arrays.sort(nums);
+        int n=nums.length;
+
+        int i=n-1;
+        long tot=0;
+        int usedk=k;
+        while(i>=0 && usedk>0 && mul>1){
+            long sum=1L*nums[i]*mul;
+            mul--;
+            usedk--;
+            tot+=sum;
+            i--;
+        }
+        int lastposofi=i;
+        int remaink=usedk;
+        while(lastposofi>=0 && remaink>0){
+            tot+=nums[lastposofi];
+            lastposofi--;
+            remaink--;
+        }
+        
+        return tot;
+    }
+}
