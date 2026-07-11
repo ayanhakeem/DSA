708d0137bbd7cb8d92ba23eb86a13b5a8d1dd890
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-11 17:05:11 +0530

    Time: 304 ms (0.64%) | Memory: 42 MB (76.36%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isThree(int n) {
+        int cnt=0;
+        for(int m=1;m<=n;m++){
+           for(int k=1;k<=n;k++){
+                if(m*k==n) cnt++;
+           } 
+        }
+        return cnt==3;
+    }
+}
+
+// n=2
+// m=1 k=1 
+// m=1 k=2 a=2 cnt=1
+// m=2 k=1 cnt=2
+// m=2 k=2 cnt=2
+
+
+// n=4
+// m=1 k=1 r=1
+// m=1 k=2 a=2 cnt=1
+// m=1 k=3 a=3 cnt=1
+// m=1 k=4  a=4 cnt=2
+
+// m=2 k=1 ,k=2,k=3,k=4 cnt=3
+// m=3 k=1 ,k=2,k=3,k=4 cnt=3
+// m=4 k=1,2,3,4 cnt=4
