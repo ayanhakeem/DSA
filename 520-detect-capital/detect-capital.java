61e304a1f788ec9dd2068b008e34f1984ac98a2a
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-14 14:03:29 +0530

    Time: 1 ms (90.50%) | Memory: 43.3 MB (46.19%) - LeetSync

--- 520-detect-capital/detect-capital.java
+++ 520-detect-capital/detect-capital.java
@@ -1,9 +1,35 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        int n=word.length();
-        if(word.equals(word.toUpperCase())) return true; 
-        if(word.equals(word.toLowerCase())) return true;
-        if(Character.isUpperCase(word.charAt(0)) && word.substring(1).equals(word.substring(1).toLowerCase())) return true;
-        return false;
+        char arr[]=word.toCharArray();
+        int n=arr.length;
+        int cc=0;
+        int sc=0;
+        for(int i=0;i<arr.length;i++){
+            if(Character.isUpperCase(arr[i])){
+                cc++;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(Character.isLowerCase(arr[i])){
+                sc++;
+            }
+        }
+        if(Character.isUpperCase(arr[0]) && sc==n-1) return true;
+        if(cc==n || sc==n) return true;
+        return false; 
     }
 }
+
+
+// class Solution {
+//     public boolean detectCapitalUse(String word) {
+//         int n=word.length();
+//         if(word.equals(word.toUpperCase())) return true; 
+//         if(word.equals(word.toLowerCase())) return true;
+//         if(Character.isUpperCase(word.charAt(0)) && word.substring(1).equals(word.substring(1).toLowerCase())) return true;
+//         return false;
+//     }
+// }
+
+// //tc=o(n)
+// //sc=o(n)
