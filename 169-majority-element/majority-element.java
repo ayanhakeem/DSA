eba39a85ffcae56d981559e46a3f9f62ca798c54
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-14 19:41:00 +0530

    Time: 13 ms (31.23%) | Memory: 52.7 MB (80.82%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,30 +1,15 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int cnt = 0; // count
-        int el = 0; // Element
-
-        //applying the algorithm:
-        for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                cnt = 1;
-                el = nums[i];
-            } else if (el == nums[i]) cnt++;
-            else cnt--;
+       Map<Integer,Integer>map=new HashMap<>();
+       for(int i=0;i<nums.length;i++){
+        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+       } 
+       int t=nums.length/2;
+       for(int i:map.keySet()){
+        if(map.get(i)>t){
+            return i;
         }
-
-        //checking if the stored element
-        // is the majority element:
-        int cnt1 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == el) cnt1++;
-        }
-
-        if (cnt1 > (n / 2)) return el;
-        return -1;
+       }
+       return -1;
     }
-
 }
-
-//tc=o(n)+o(n)
-//sc=o(1)
