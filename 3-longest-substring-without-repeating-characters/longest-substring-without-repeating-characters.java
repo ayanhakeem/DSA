8a3643b82a0080c7dc34505b58557e4853a90439
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-14 19:12:46 +0530

    Time: 2 ms (97.53%) | Memory: 45.4 MB (77.79%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -20,7 +20,7 @@ class Solution {
             
             if (hash[s.charAt(r)] != -1) {
                 
-                if (hash[s.charAt(r)] >= l) {
+                if (hash[s.charAt(r)] >= l) {//the prev/last occurence of curr shoulb be within tange(l-r) means >=l not beyond
                     
                     l = hash[s.charAt(r)] + 1;
                 }
@@ -45,3 +45,18 @@ class Solution {
 
 //tc=o(n)
 //sc=o(256)
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int n=s.length();
+//         Map<Character,Integer>map=new HashMap<>();
+//         int cnt=0;
+//         for(int i=0;i<n;i++){
+//             if(!map.containsKey(s.charAt(i))){
+//                 map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// }
