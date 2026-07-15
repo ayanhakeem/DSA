86f57cc85de432a360a3c8666244eaab7b8bebd5
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-15 11:16:00 +0530

    Time: 5 ms (64.59%) | Memory: 43.6 MB (38.55%) - LeetSync

---
+++ 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String greatestLetter(String s) {
+        int n=s.length();
+        int upper[]=new int[26];
+        int lower[]=new int[26];
+
+        for(int i=0;i<n;i++){
+          char ch=s.charAt(i);
+          if(Character.isUpperCase(ch)){
+            upper[ch-'A']++;
+          }else if(Character.isLowerCase(ch)){
+            lower[ch-'a']++;
+          }
+        }
+
+        int l=25,r=25;
+        while(l>=0 && r>=0){
+            if(upper[l]>0 && lower[r]>0){
+                return String.valueOf((char)(l+'A'));
+            }
+            l--;
+            r--;
+        }
+        return "";
+    }
+}
