1ad00ae1d5b2bc52fba0efa4dad0ca4a6479399a
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-15 14:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (44.99%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.java
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -1,23 +1,51 @@
 class Solution {
     public boolean checkRecord(String s) {
-        int n=s.length();
-        int cnta=0;
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)=='A'){
-                cnta++;
+        int cntA = 0;
+        int cntL = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'A') {
+                cntA++;
+                cntL = 0;   // break L streak
+            } else if (s.charAt(i) == 'L') {
+                cntL++;
+            } else {
+                cntL = 0;   // 'P' breaks L streak
+            }
+
+            if (cntA >= 2 || cntL >= 3) {
+                return false;
             }
         }
-        int cntl=1;
-        int max=1;
-        for(int i=1;i<n;i++){
-            if(s.charAt(i-1)=='L' && s.charAt(i)=='L'){
-                cntl++;
-                max=Math.max(cntl,max);
-            }else{
-                cntl=1;
-            }
-        }
-        if(cnta<2 && max<3) return true;
-        return false;
+
+        return true;
     }
 }
+// class Solution {
+//     public boolean checkRecord(String s) {
+//         int n=s.length();
+//         int cnta=0;
+//         for(int i=0;i<n;i++){
+//             if(s.charAt(i)=='A'){
+//                 cnta++;
+//             }
+//         }
+//         int cntl=1;
+//         int max=1;
+//         for(int i=1;i<n;i++){
+//             if(s.charAt(i-1)=='L' && s.charAt(i)=='L'){
+//                 cntl++;
+//                 max=Math.max(cntl,max);
+//             }else{
+//                 cntl=1;
+//             }
+//         }
+//         if(cnta<2 && max<3) return true;
+//         return false;
+//     }
+// }
+// llall
+// cntl=2 then reset cnt=1;
+
+//tc=o(n)+o(n)
+//sc=o(1)
