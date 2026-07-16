6c4d60ae5fe2f110828b0eb053d03830955ddaf1
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-16 14:36:54 +0530

    Time: 27 ms (25.23%) | Memory: 47.1 MB (16.90%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,30 +1,54 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        HashSet<Character>set=new HashSet<>();
-        for(char c:s.toCharArray()){
-            set.add(c);
-        }
-        int res=0;
+        Map<Character,Integer>map=new HashMap<>();
         int n=s.length();
-        for(char c:set){
-            int cnt=0;
-            int l=0;
-            for(int r=0;r<n;r++){
-                if(s.charAt(r)==c){
-                    cnt++;
-                }
-                while((r-l+1)-cnt>k){
-                    if(s.charAt(l)==c){
-                        cnt--;
-                    }
-                    l++;
-                }
-                res=Math.max(res,r-l+1);
+        int res=0;
+        int maxf=0;
+        int l=0;
+        for(int r=0;r<n;r++){
+            map.put(s.charAt(r),map.getOrDefault(s.charAt(r),0)+1);
+            maxf=Math.max(maxf,map.get(s.charAt(r)));
+
+            while((r-l+1)-maxf>k){
+                map.put(s.charAt(l),map.get(s.charAt(l))-1);
+                l++;
             }
+            res=Math.max(res,r-l+1);
         }
         return res;
     }
 }
+
+// class Solution {
+//     public int characterReplacement(String s, int k) {
+//         HashSet<Character>set=new HashSet<>();
+//         for(char c:s.toCharArray()){
+//             set.add(c);
+//         }
+//         int res=0;
+//         int n=s.length();
+//         for(char c:set){
+//             int cnt=0;
+//             int l=0;
+//             for(int r=0;r<n;r++){
+//                 if(s.charAt(r)==c){
+//                     cnt++;
+//                 }
+//                 while((r-l+1)-cnt>k){
+//                     if(s.charAt(l)==c){
+//                         cnt--;
+//                     }
+//                     l++;
+//                 }
+//                 res=Math.max(res,r-l+1);
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+//tc=o(m*n)
+//sc=o(n)
 
 
 //brute force
