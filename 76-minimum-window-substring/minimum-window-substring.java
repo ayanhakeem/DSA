868d37fe2bbc88540bc29a028c2c42e23467d859
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-18 12:08:34 +0530

    Time: 19 ms (42.55%) | Memory: 48 MB (14.10%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,60 +1,42 @@
-import java.util.*;
-
-public class Solution {
-    // Function to find the minimum window substring
+class Solution {
     public String minWindow(String s, String t) {
-        // Frequency map for characters in t
-        Map<Character, Integer> targetFreq = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            targetFreq.put(c, targetFreq.getOrDefault(c, 0) + 1);
+        Map<Character,Integer>targetfreq=new HashMap<>();
+        for(char c:t.toCharArray()){
+            targetfreq.put(c,targetfreq.getOrDefault(c,0)+1);
         }
 
-        // Number of unique characters required to match
-        int required = targetFreq.size();
+        int l=0,r=0;
+        int minlen=Integer.MAX_VALUE;
+        int formed=0;
+        int required=targetfreq.size();
+        int minleft=0;
 
-        // Left and right window pointers
-        int left = 0, right = 0;
+        Map<Character,Integer>windowfreq=new HashMap<>();
 
-        // Tracks how many characters in current window match required frequency
-        int formed = 0;
 
-        // Frequency map for the current window
-        Map<Character, Integer> windowFreq = new HashMap<>();
-
-        // Minimum window length and starting index
-        int minLen = Integer.MAX_VALUE;
-        int minLeft = 0;
-
-        // Expand the window
-        while (right < s.length()) {
-            char c = s.charAt(right);
-            windowFreq.put(c, windowFreq.getOrDefault(c, 0) + 1);
-
-            // If character matches target frequency
-            if (targetFreq.containsKey(c) && windowFreq.get(c).intValue() == targetFreq.get(c).intValue()) {
+        while(r<s.length()){
+            char c=s.charAt(r);
+            windowfreq.put(c, windowfreq.getOrDefault(c, 0) + 1);
+            if(targetfreq.containsKey(c) && targetfreq.get(c).intValue()==windowfreq.get(c).intValue()){
                 formed++;
             }
-
-            // Try shrinking the window if all target characters matched
-            while (left <= right && formed == required) {
-                if ((right - left + 1) < minLen) {
-                    minLen = right - left + 1;
-                    minLeft = left;
+            while(l<=r && formed==required){
+                if((r-l+1)<minlen){
+                    minlen=r-l+1;
+                    minleft=l;
                 }
 
-                char leftChar = s.charAt(left);
-                windowFreq.put(leftChar, windowFreq.get(leftChar) - 1);
-
-                if (targetFreq.containsKey(leftChar) && windowFreq.get(leftChar) < targetFreq.get(leftChar)) {
+                char leftchar=s.charAt(l);
+                windowfreq.put(leftchar,windowfreq.get(leftchar)-1);
+                if (targetfreq.containsKey(leftchar) && windowfreq.get(leftchar) < targetfreq.get(leftchar)) {
                     formed--;
                 }
 
-                left++;
+                l++;
+
             }
-
-            right++;
+            r++;
         }
-
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLen);
+        return minlen == Integer.MAX_VALUE ? "" : s.substring(minleft, minleft + minlen);
     }
 }
