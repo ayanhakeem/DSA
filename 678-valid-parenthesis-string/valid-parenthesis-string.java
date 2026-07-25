d73366b0e5a5a5d58a33408dfefee44503dbe327
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-26 00:44:00 +0530

    Time: 1 ms (27.86%) | Memory: 42.9 MB (19.78%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer>left=new Stack<>();
+        Stack<Integer>star=new Stack<>();
+        int n=s.length();
+
+        for(int i=0;i<n;i++){
+            char c=s.charAt(i);
+            if(c=='('){
+                left.push(i);
+            }else if(c=='*'){
+                star.push(i);
+            }else{
+                if(left.isEmpty() && star.isEmpty()) return false;
+                if(!left.isEmpty()){
+                    left.pop();
+                }else{
+                    star.pop();
+                }
+            }
+        }
+        while(!left.isEmpty() && !star.isEmpty()){
+            if(left.pop()>star.pop()){
+                return false;
+            }
+        }
+        return left.isEmpty();
+
+    }
+}
