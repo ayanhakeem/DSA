6dc63efb96c43a567e6b4c19beb34fa797bca9b0
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-26 23:41:25 +0530

    Time: 5 ms (71.10%) | Memory: 168.7 MB (64.45%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -1,16 +1,39 @@
+// class Solution {
+//     public boolean mergeTriplets(int[][] triplets, int[] target) {
+//        Set<Integer>good=new HashSet<>();
+//        for(int t[]:triplets){
+//          if(t[0]>target[0] || t[1]>target[1] || t[2]>target[2]){
+//             continue;
+//          }
+//          for(int i=0;i<t.length;i++){
+//             if(t[i]==target[i]){
+//                 good.add(i);
+//             }
+//          }
+//        }
+//        return good.size()==3;
+//     }
+// }
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
-       Set<Integer>good=new HashSet<>();
-       for(int t[]:triplets){
-         if(t[0]>target[0] || t[1]>target[1] || t[2]>target[2]){
-            continue;
-         }
-         for(int i=0;i<t.length;i++){
-            if(t[i]==target[i]){
-                good.add(i);
+        boolean a = false, b = false, c = false; 
+
+        for (int i = 0; i < triplets.length; i++) {
+            if (triplets[i][0] > target[0] || 
+                triplets[i][1] > target[1] ||
+                triplets[i][2] > target[2]) {
+                    continue;
+                }
+            
+            for (int j = 0; j < 3; j++) {
+                if (triplets[i][j] == target[j]) {
+                    if (j == 0) a = true; 
+                    if (j == 1) b = true; 
+                    if (j == 2) c = true; 
+                }
             }
-         }
-       }
-       return good.size()==3;
+        }
+
+        return a && b && c;
     }
 }
