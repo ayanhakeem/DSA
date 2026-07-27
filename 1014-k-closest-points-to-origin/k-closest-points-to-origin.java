115ee1eb1d0cc5a74fa3d03a23e29139423fe6e7
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-27 15:10:32 +0530

    Time: 30 ms (66.60%) | Memory: 53.5 MB (84.18%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,36 +1,56 @@
 class Solution {
-    static class point implements Comparable<point>{
-        int x;
-        int y;
-        int distsq;
-
-        public point(int x,int y,int distsq){
-            this.x=x;
-            this.y=y;
-            this.distsq=distsq;
-        }
-        @Override
-        public int compareTo(point p2){
-            return p2.distsq-this.distsq;
-        }
-    }
     public int[][] kClosest(int[][] points, int k) {
-        int n=points.length;
-        PriorityQueue<point>pq=new PriorityQueue<>();
-        for(int i=0;i<n;i++){
-            int distsq=points[i][0]*points[i][0]+points[i][1]*points[i][1];
-             pq.add(new point(points[i][0],points[i][1],distsq));
-
-            if(pq.size()>k){
-                pq.remove();
+        PriorityQueue<int[]>maxheap=new PriorityQueue<>((a,b) -> (b[0]*b[0]-a[0]*a[0])+(b[1]*b[1]-a[1]*a[1]));
+        for(int p[]:points){
+            maxheap.add(p);
+            if(maxheap.size()>k){
+                maxheap.remove();
             }
         }
-        int arr[][]=new int[k][2];
+        int ans[][]=new int[k][2];
         for(int i=0;i<k;i++){
-            point p=pq.remove();
-            arr[i][0]=p.x;
-            arr[i][1]=p.y;
+            ans[i]=maxheap.remove();
         }
-        return arr;
+
+        return ans;
+        
     }
 }
+
+//maxheap solution
+// class Solution {
+//     static class point implements Comparable<point>{
+//         int x;
+//         int y;
+//         int distsq;
+
+//         public point(int x,int y,int distsq){
+//             this.x=x;
+//             this.y=y;
+//             this.distsq=distsq;
+//         }
+//         @Override
+//         public int compareTo(point p2){
+//             return p2.distsq-this.distsq;
+//         }
+//     }
+//     public int[][] kClosest(int[][] points, int k) {
+//         int n=points.length;
+//         PriorityQueue<point>pq=new PriorityQueue<>();
+//         for(int i=0;i<n;i++){
+//             int distsq=points[i][0]*points[i][0]+points[i][1]*points[i][1];
+//              pq.add(new point(points[i][0],points[i][1],distsq));
+
+//             if(pq.size()>k){
+//                 pq.remove();
+//             }
+//         }
+//         int arr[][]=new int[k][2];
+//         for(int i=0;i<k;i++){
+//             point p=pq.remove();
+//             arr[i][0]=p.x;
+//             arr[i][1]=p.y;
+//         }
+//         return arr;
+//     }
+// }
