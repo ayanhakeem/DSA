59bc41b9bd55dfb8c39b92abfae738afdaffe92a
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-29 13:03:16 +0530

    Time: 1 ms (99.96%) | Memory: 42.6 MB (67.84%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,24 +1,52 @@
 class Solution {
     public long sumAndMultiply(int n) {
-        Stack<Integer>st=new Stack<>();
         long sum=0;
+        long res=0;
         while(n>0){
-            int ld=n%10;
+            long ld=n%10;
             if(ld!=0){
-                st.push(ld);
                 sum+=ld;
+                res=ld+res*10;
             }
-
             n/=10;
         }
-
-        long res=0;
-        while(!st.isEmpty()){
-            res=st.pop()+res*10;
+        long newnum=res;
+        long finalres=0;
+        while(newnum>0){
+            long ld=newnum%10;
+            finalres=ld+finalres*10;
+            newnum=newnum/10;
         }
 
-        return res*sum;
+        return finalres*sum;
 
 
     }
 }
+
+// class Solution {
+//     public long sumAndMultiply(int n) {
+//         Stack<Integer>st=new Stack<>();
+//         long sum=0;
+//         while(n>0){
+//             int ld=n%10;
+//             if(ld!=0){
+//                 st.push(ld);
+//                 sum+=ld;
+//             }
+
+//             n/=10;
+//         }
+
+//         long res=0;
+//         while(!st.isEmpty()){
+//             res=st.pop()+res*10;
+//         }
+
+//         return res*sum;
+
+
+//     }
+// }
+//tc=o(n)
+//sc=o(n)
