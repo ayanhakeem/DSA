29f9c5d4bbb04701353dedcb57771d5b92c2c8d4
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-07-30 12:46:34 +0530

    Time: 3 ms (69.57%) | Memory: 49.5 MB (12.36%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -3,15 +3,28 @@ class Solution {
         int n=arr.length;
         int ans[]=new int[n];
         ans[n-1]=-1;
-        for(int i=0;i<n-1;i++){
-            int max=Integer.MIN_VALUE;
-            for(int j=i+1;j<n;j++){
-                if(arr[j]>max){
-                    max=arr[j];
-                }
-            }
+        int max=arr[n-1];
+        for(int i=n-2;i>=0;i--){
             ans[i]=max;
+            max=Math.max(max,arr[i]);
         }
         return ans;
     }
 }
+// class Solution {
+//     public int[] replaceElements(int[] arr) {
+//         int n=arr.length;
+//         int ans[]=new int[n];
+//         ans[n-1]=-1;
+//         for(int i=0;i<n-1;i++){
+//             int max=Integer.MIN_VALUE;
+//             for(int j=i+1;j<n;j++){
+//                 if(arr[j]>max){
+//                     max=arr[j];
+//                 }
+//             }
+//             ans[i]=max;
+//         }
+//         return ans;
+//     }
+// }
