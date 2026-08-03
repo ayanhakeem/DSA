9260c3e4952af1a824fc1559a80d5f271d9425cf
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-08-03 16:48:57 +0530

    Time: 6 ms (96.92%) | Memory: 47.9 MB (16.94%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,21 +10,50 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-       if(root==null || root==p || root==q) return root;
+        if(root==null) return null;
+        int curr=root.val;
+        if(curr<p.val && curr<q.val){
+            return lowestCommonAncestor(root.right, p,  q);
 
-        TreeNode left=lowestCommonAncestor(root.left,p,q);
-        TreeNode right=lowestCommonAncestor(root.right,p,q);
-
-        if(left==null){
-            return right;
         }
-        else if(right==null){
-            return left;
-        }else{
-            return root;//it is ancestor
+        if(curr>p.val && curr>q.val){
+            return lowestCommonAncestor(root.left, p,  q);
         }
-
-
-
+        
+        return root;
+        
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if(root==null || root==p || root==q) return root;
+
+//         TreeNode left=lowestCommonAncestor(root.left,p,q);
+//         TreeNode right=lowestCommonAncestor(root.right,p,q);
+
+//         if(left==null){
+//             return right;
+//         }
+//         else if(right==null){
+//             return left;
+//         }else{
+//             return root;//it is ancestor
+//         }
+
+
+
+//     }
+// }
+//tc=o(N)
+//sc=o(h) wc=o(n)
