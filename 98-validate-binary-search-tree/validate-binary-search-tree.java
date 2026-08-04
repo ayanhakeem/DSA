be4017281fd73c0daba41eb48609676694650200
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-08-04 13:32:29 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (34.97%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,15 +14,31 @@
  * }
  */
  //
-class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
         
-    }
-    public boolean valid(TreeNode root,long minval,long maxval){
-        if(root==null) return true;
-        if(root.val>=maxval || root.val<=minval) return false;
-        return valid(root.left,minval,root.val) && valid(root.right,root.val,maxval);
-    }
+//     }
+//     public boolean valid(TreeNode root,long minval,long maxval){
+//         if(root==null) return true;
+//         if(root.val>=maxval || root.val<=minval) return false;
+//         return valid(root.left,minval,root.val) && valid(root.right,root.val,maxval);
+//     }
     
+// }
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean valid(TreeNode node, long left, long right) {
+        if (node == null) {
+            return true;
+        }
+        if (!(left < node.val && node.val < right)) {
+            return false;
+        }
+        return valid(node.left, left, node.val) &&
+               valid(node.right, node.val, right);
+    }
 }
