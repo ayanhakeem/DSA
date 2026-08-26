01fb409907587cd0167a263b70437fa875f3f2f9
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-08-26 10:34:56 +0530

    Time: 2 ms (44.68%) | Memory: 47.4 MB (61.23%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -6,19 +6,36 @@ class Solution {
                 ans.add(j);
             }
         }
-        int res[]=new int[2];
-        Map<Integer,Integer>map=new HashMap<>();
-        for(int i=0;i<ans.size();i++){
-            map.put(ans.get(i),map.getOrDefault(ans.get(i),0)+1);
+         // Size of the array
+        long n = ans.size();
+
+        // Sum of first n natural numbers
+        long SN = (n * (n + 1)) / 2;
+
+        // Sum of squares of first n natural numbers
+        long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+        // Calculate actual sum (S) and sum of squares (S2) of array elements
+        long S = 0, S2 = 0;
+        for (int i = 0; i < n; i++) {
+            S += ans.get(i);
+            S2 += (long) ans.get(i) * (long) ans.get(i);
         }
-        for(int i=1;i<=grid.length*grid.length;i++){
-            if(!map.containsKey(i)){
-                res[1]=i;
-            }
-            else if(map.get(i)==2){
-                res[0]=i;
-            }
-        }
-        return res;
+
+        // Compute the difference values
+        long val1 = S - SN; // X - Y
+
+        // S2 - S2n = X^2 - Y^2
+        long val2 = S2 - S2N;
+
+        // Calculate X + Y
+        val2 = val2 / val1;
+
+        // Calculate X and Y
+        long x = (val1 + val2) / 2; // repeating
+        long y = x - val1;          // missing
+
+        return new int[]{(int) x, (int) y};
     }
 }
+    
