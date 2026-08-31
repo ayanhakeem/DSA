c0a49a3b0b0ce2743b538f9d6e591cb9036c51c1
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-08-31 23:34:38 +0530

    Time: 265 ms (38.85%) | Memory: 43.7 MB (46.19%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.java
+++ 686-repeated-string-match/repeated-string-match.java
@@ -1,31 +1,49 @@
 class Solution {
-    public boolean issub(String a,String b){
-        for(int i=0;i<=a.length()-b.length();i++){
-            if(a.substring(i,i+b.length()).equals(b)){
-                return true;
-            }
-        }
-        return false;
-    }
     public int repeatedStringMatch(String a, String b) {
-        StringBuilder sb=new StringBuilder();
-        int cnt=0;
-        while(sb.length()<b.length()){//until a.length=b.length
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        while (sb.length() < b.length()) {
             sb.append(a);
-            cnt++;
+            count++;
         }
 
-       if(sb.toString().contains(b)) return cnt;
-        //extra adding  beacuse may b starts at other idx means we are repeting a until a.length()==b.length() so we assume as the b's starting idx starts from 0th idx of a but may starts from others so extra repetation required
+        if (sb.indexOf(b) != -1) return count;
+
         sb.append(a);
-        cnt++;
+        if (sb.indexOf(b) != -1) return count+1;
 
-        if(sb.toString().contains(b)){
-            return cnt;
-        }
         return -1;
     }
 }
+
+// class Solution {
+//     public boolean issub(String a,String b){
+//         for(int i=0;i<=a.length()-b.length();i++){
+//             if(a.substring(i,i+b.length()).equals(b)){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+//     public int repeatedStringMatch(String a, String b) {
+//         StringBuilder sb=new StringBuilder();
+//         int cnt=0;
+//         while(sb.length()<b.length()){//until a.length=b.length
+//             sb.append(a);
+//             cnt++;
+//         }
+
+//        if(sb.toString().contains(b)) return cnt;
+//         //extra adding  beacuse may b starts at other idx means we are repeting a until a.length()==b.length() so we assume as the b's starting idx starts from 0th idx of a but may starts from others so extra repetation required
+//         sb.append(a);
+//         cnt++;
+
+//         if(sb.toString().contains(b)){
+//             return cnt;
+//         }
+//         return -1;
+//     }
+// }
 
 
 // class Solution {
@@ -59,3 +77,4 @@ class Solution {
 //         return -1;
 //     }
 // }
+//tc=o(a-b)*o(a-b)*b
