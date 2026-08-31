a712682daa9d40b7ca6211be6df54b9e521b147d
Mahmadayan hakeem <165511523+ayanhakeem@users.noreply.github.com>
2026-08-31 23:20:39 +0530

    Time: 283 ms (6.96%) | Memory: 47.2 MB (5.89%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean issub(String a,String b){
+        for(int i=0;i<=a.length()-b.length();i++){
+            if(a.substring(i,i+b.length()).equals(b)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public int repeatedStringMatch(String a, String b) {
+        StringBuilder sb=new StringBuilder();
+        int cnt=0;
+        while(sb.length()<b.length()){
+            sb.append(a);
+            cnt++;
+        }
+
+        if(issub(sb.toString(),b)){
+            return cnt;
+        }
+        //extra adding  beacuse may b starts at other idx means we are repeting a until a.length()==b.length() so we assume as the b's starting idx starts from 0th idx of a but may starts from others so extra repetation required
+        sb.append(a);
+        cnt++;
+
+        if(issub(sb.toString(),b)){
+            return cnt;
+        }
+        return -1;
+    }
+}
